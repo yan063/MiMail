bee3a6b0f811aa380e5bc9d046c9f6cfac2218cd
苟兵燕 <goubingyan@hudunsoft.com>
2022-06-19 17:18:38 +0800

    Vue2.0

--- shopping/src/router.js
+++ shopping/src/router.js
@@ -2,84 +2,94 @@ import Vue from "vue"
 import Router from "vue-router"
 import Home from "./pages/home"
 import Index from "./pages/index"
-import Product from "./pages/product"
-import Detail from "./pages/detail"
-import Order from "./pages/order"
-import OrderList from "./pages/orderList"
-import OrderPay from "./pages/orderPay"
-import OrderConfirm from "./pages/orderConfirm"
-import OrderAlipay from "./pages/orderAlipay"
-import Login from "./pages/login"
-import Register from "./pages/register"
-import Cart from "./pages/cart"
+// import Product from "./pages/product"
+// import Detail from "./pages/detail"
+// import Order from "./pages/order"
+// import OrderList from "./pages/orderList"
+// import OrderPay from "./pages/orderPay"
+// import OrderConfirm from "./pages/orderConfirm"
+// import OrderAlipay from "./pages/orderAlipay"
+// import Login from "./pages/login"
+// import Register from "./pages/register"
+// import Cart from "./pages/cart"
 
 Vue.use(Router);
 
- export default new Router({
-     routes:[{
-        path:'/',
-        name:'home',
-        redirect:'index',//默认页面
-        component:Home,
-        children:[
+export default new Router({
+    routes: [{
+        path: '/',
+        name: 'home',
+        redirect: 'index',//默认页面
+        component: Home,
+        children: [
             {
-                path:'/index',
-                name:'index',
-                component:Index,
+                path: '/index',
+                name: 'index',
+                component: Index,
 
-            },{
-                path:'/product/:id',
-                name:'product',
-                component:Product,
+            }, {
+                path: '/product/:id',
+                name: 'product',
+                component: () => import('./pages/product.vue'),
             },
             {
-                path:'/detail/:id',
-                name:'detail',
-                component:Detail,
+                path: '/detail/:id',
+                name: 'detail',
+                component: () => import('./pages/detail.vue'),
             }
         ]
-     },{
-        path:'/login',
-        name:'login',
-        component:Login,
-     },
-     {
-        path:'/register',
-        name:'register',
-        component:Register,
-     },
-     {
-        path:'/cart',//购物车界面
-        name:'cart',
-        component:Cart,
-     },
-     {
-        path:'/order',
-        name:'order',
-        component:Order,
-        children:[
+    }, {
+        path: '/login',
+        name: 'login',
+        // component:resolve=>require(['./pages/login.vue'],resolve),
+        component: () => import('./pages/login.vue'),
+    },
+    {
+        path: '/register',
+        name: 'register',
+        component: () => import('./pages/register.vue'),
+    },
+    {
+        path: '/cart',//购物车界面
+        name: 'cart',
+        // component:Cart,
+        component: () => import('./pages/cart.vue'),
+    },
+    {
+        path: '/order',
+        name: 'order',
+        component: () => import('./pages/order.vue'),
+        children: [
             {
-                path:'confirm',
-                name:'order-confirm',
-                component:OrderConfirm,
-            },{
-                path:'list',
-                name:'order-list',
-                component:OrderList,
+                path: 'confirm',
+                name: 'order-confirm',
+                // component:OrderConfirm,
+                component: () => import('./pages/orderConfirm.vue'),
+
+            }, {
+                path: 'list',
+                name: 'order-list',
+                // component:OrderList,
+                component: () => import('./pages/orderList.vue'),
+
             },
             {
-                path:'pay',
-                name:'order-pay',
-                component:OrderPay,
+                path: 'pay',
+                name: 'order-pay',
+                // component:OrderPay,
+                component: () => import('./pages/orderPay.vue'),
+
             },
             {
-                path:'alipay',
-                name:'alipay',
-                component:OrderAlipay,
+                path: 'alipay',
+                name: 'alipay',
+                // component:OrderAlipay,
+                component: () => import('./pages/orderAlipay.vue'),
+
             }
-        ]         
-     }
+        ]
+    }
 
-     ]
+    ]
 
- })
+})
--- shopping/vue.config.js
+++ shopping/vue.config.js
@@ -15,5 +15,13 @@ module.exports={
     }
     ,
     // lintOnSave:false;关闭link校验
+    // publicPath:'/app',//打包前需要确认项目是否在根目录下，如果不在，可以加一个publicPath为二级目录，以便项目可以访问
+    // outputDir:"dest",//打包后的项目目录名字
+    // indexPath:'index2.html',//打包后页面的名字
+    // filenameHashing:false,//浏览器带有缓存，设置为false,将关闭缓存，一般为true
+    productionSourceMap:true,
+    chainWebpack:(config)=>{//删除路由懒加载的预加载
+        config.plugins.delete('prefetch');
+    }
 
 }
